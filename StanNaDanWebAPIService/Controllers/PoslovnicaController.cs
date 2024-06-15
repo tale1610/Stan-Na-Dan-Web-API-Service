9109dd4a047f768de188b52a64a537700da17a8b
taskov_ <taskovvukan@gmail.com>
2024-06-15 18:29:12 +0200

    CORS

---
+++ StanNaDanWebAPIService/Controllers/PoslovnicaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace StanNaDanWebAPIService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PoslovnicaController : ControllerBase
+    {
+        [HttpGet("VratiSvePoslovnice")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<ActionResult> VratiSvePoslovnice()
+        {
+            var (isError, odeljenja, error) = await DataProvider.VratiSvaOdeljenjaAsync();
+
+            if (isError)
+            {
+                return StatusCode(error?.StatusCode ?? 400, error?.Message);
+            }
+
+            return Ok(odeljenja);
+        }
+    }
+}
